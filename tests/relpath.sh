2ef82efa1906eeb3c7ad8f9c7002dc4836f4e0df
aabccd021 <aabccd021@gmail.com>
2025-02-24 19:07:20 +0700

    fix: Add error handling for systemd-notify-fifo script

--- tests/relpath.sh
+++ tests/relpath.sh
@@ -1,7 +1,14 @@
 NOTIFY_SOCKET=./notify.sock
 export NOTIFY_SOCKET
 
-systemd-notify-fifo ./notify.pipe 2>./actual.txt || true
+exit_code=0
+systemd-notify-fifo ./notify.pipe 2>./actual.txt || exit_code=$?
+
+if [ "$exit_code" -ne 1 ]; then
+  echo "Expected: 1"
+  echo "Got: $exit_code"
+  exit 1
+fi
 
 echo "NOTIFY_SOCKET must be an absolute path, otherwise systemd-notify will throw Protocol error" >expected.txt
 diff --unified --color expected.txt actual.txt
