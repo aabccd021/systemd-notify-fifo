8e2b06dad1e900525d9fd8c25a5a1c5449d5183f
aabccd021 <aabccd021@gmail.com>
2025-02-26 19:25:28 +0700

    Update flake.nix with systemd-notify-fifo additions

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+
+  nixConfig.allow-import-from-derivation = false;
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     treefmt-nix = {
@@ -9,7 +12,32 @@
 
   outputs = { self, nixpkgs, treefmt-nix }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      overlay = (final: prev:
+        let
+          systemd-notify-fifo-server = final.runCommandLocal "systemd-notify-fifo" { } ''
+            mkdir -p "$out/bin" 
+            export XDG_CACHE_HOME="$PWD"
+            ${final.go}/bin/go build -o "$out/bin/systemd-notify-fifo-server" ${./server.go}
+          '';
+
+          systemd-notify-fifo = final.writeShellApplication {
+            name = "systemd-notify-fifo";
+            runtimeInputs = [ systemd-notify-fifo-server ];
+            text = builtins.readFile ./systemd_notify_fifo.sh;
+          };
+
+        in
+        {
+          systemd-notify-fifo-server = systemd-notify-fifo-server;
+          systemd-notify-fifo = systemd-notify-fifo;
+
+        });
+
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlay ];
+      };
 
       treefmtEval = treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
@@ -22,41 +50,34 @@
         settings.global.excludes = [ "LICENSE" ];
       };
 
-      systemd-notify-fifo-server = pkgs.runCommandLocal "systemd-notify-fifo" { } ''
-        mkdir -p "$out/bin" 
-        export XDG_CACHE_HOME="$PWD"
-        ${pkgs.go}/bin/go build -o "$out/bin/systemd-notify-fifo-server" ${./server.go}
-      '';
-
-      systemd-notify-fifo = pkgs.writeShellApplication {
-        name = "systemd-notify-fifo";
-        runtimeInputs = [ systemd-notify-fifo-server ];
-        text = builtins.readFile ./systemd_notify_fifo.sh;
-      };
 
       testAttrs = import ./tests {
         pkgs = pkgs;
-        systemd-notify-fifo = systemd-notify-fifo;
-        systemd-notify-fifo-server = systemd-notify-fifo-server;
+        systemd-notify-fifo = pkgs.systemd-notify-fifo;
+        systemd-notify-fifo-server = pkgs.systemd-notify-fifo-server;
       };
 
       tests = pkgs.lib.mapAttrs' (name: value: { name = "test-" + name; value = value; }) testAttrs;
 
       packages = tests // {
-        systemd-notify-fifo-server = systemd-notify-fifo-server;
-        systemd-notify-fifo = systemd-notify-fifo;
+        systemd-notify-fifo-server = pkgs.systemd-notify-fifo-server;
+        systemd-notify-fifo = pkgs.systemd-notify-fifo;
         formatting = treefmtEval.config.build.check self;
       };
 
       gcroot = packages // {
         gcroot-all = pkgs.linkFarm "gcroot-all" packages;
-        default = systemd-notify-fifo;
+        default = pkgs.systemd-notify-fifo;
       };
 
     in
     {
       packages.x86_64-linux = gcroot;
+
       checks.x86_64-linux = gcroot;
+
       formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      overlays.default = overlay;
     };
 }
