e8690f76438bebcb573994eda4dd76065dde9456
aabccd021 <aabccd021@gmail.com>
2025-03-15 15:04:24 +0700

    Update dependencies and configurations in flake.lock and flake.nix files

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
       };
 
       gcroot = packages // {
-        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+        gcroot = pkgs.linkFarm "gcroot" packages;
         default = pkgs.systemd-notify-fifo;
       };
 
